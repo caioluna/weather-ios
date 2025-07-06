289219cc29a51beb0af4c95f3b848d0fe74cbab5
Caio Luna <ccluna@gmail.com>
2025-07-05 22:24:04 -0300

    feat: Daily Forecast Table View, UIView extensions

---
+++ WeatherApp/DailyForecastTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DailyForecastTableViewCell.swift
+//  WeatherApp
+//
+//  Created by Caio Luna on 05/07/25.
+//
+
+import UIKit
+
+class DailyForecastTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ WeatherApp/UIView+Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  UIView+Extensions.swift
+//  WeatherApp
+//
+//  Created by Caio Luna on 05/07/25.
+//
+
+import Foundation
